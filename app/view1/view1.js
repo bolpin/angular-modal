ca5409d92275429c504b74832d16693de46a5f56
bolpin <bolpin@gettyimages.com>
2017-04-10 17:44:55 -0700

    paging added

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,11 +13,29 @@ angular.module('myApp.view1', ['ngRoute'])
   $scope.modalShown = false;
   $scope.inviteModalShown = false;
   $scope.errors = [];
+  $scope.pageSize = "5";
+  $scope.currentPage = 0;
 
   $scope.items = [
+    {'score' : 0, 'name' : 'Mrs. White'},
+    {'score' : 0, 'name' : 'Mr. Green'},
+    {'score' : 0, 'name' : 'Miss Scarlet'},
     {'score' : 0, 'name' : 'Professor Plum'},
     {'score' : 0, 'name' : 'Mrs. Peacock'},
     {'score' : 0, 'name' : 'Colonel Mustard'},
+    {'score' : 0, 'name' : 'Agustus'},
+    {'score' : 0, 'name' : 'Nero'},
+    {'score' : 0, 'name' : 'Caligula'},
+    {'score' : 0, 'name' : 'Claudius'},
+    {'score' : 0, 'name' : 'Tiberius'},
+    {'score' : 0, 'name' : 'Trajan'},
+    {'score' : 0, 'name' : 'Commodus'},
+    {'score' : 0, 'name' : 'Domitian'},
+    {'score' : 0, 'name' : 'Publius Aelius Hadrianus'},
+    {'score' : 0, 'name' : 'Marcus Aurelius'},
+    {'score' : 0, 'name' : 'Vespasian'},
+    {'score' : 0, 'name' : 'Nerva'},
+    {'score' : 0, 'name' : 'Titus'},
   ];
 
   $scope.resetErrors = function() {
@@ -31,8 +49,22 @@ angular.module('myApp.view1', ['ngRoute'])
     }
   }
 
-  $scope.increment = function(index) {
+  $scope.remove = function(items, indexOnPage) {
     $scope.resetErrors();
+
+    var index = indexOnPage + $scope.pageSize * $scope.currentPage;
+
+    items[index].spinner = true;
+
+    setTimeout(function() {
+      items.splice(index,1);
+      $scope.$apply();
+    }, 500);
+  };
+
+  $scope.increment = function(indexOnPage) {
+    $scope.resetErrors();
+    var index = indexOnPage + $scope.pageSize * $scope.currentPage;
     $scope.items[index].spinner = true;
     setTimeout(function() {
       $scope.items[index].score++;
@@ -52,4 +84,19 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.inviteModalShown = true;
   };
 
-}]);
+  $scope.numberOfPages=function(){
+    return Math.ceil($scope.items.length/$scope.pageSize);
+  }
+
+  $scope.$watch('pageSize', function(newValue,oldValue){
+    $scope.currentPage = 0;
+  },true);
+
+}])
+
+.filter('startFrom', function() {
+  return function(input, start) {
+    start = +start; //parse to int
+    return input.slice(start);
+  }
+});
